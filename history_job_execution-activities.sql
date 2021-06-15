8324ccead86709bd7063d3842eaf6ab0bf9b3c7e
arbenatti <arbenatti@gmail.com>
2021-06-15 15:28:28 -0300

    Create history_job_execution-activities.sql

---
+++ history_job_execution-activities.sql
@@ -0,0 +1,17 @@
+--Histórico da atividade da execução dos jobs
+SELECT 
+    A.job_id,
+    A.[name],
+    B.session_id,
+    B.run_requested_date,
+    B.run_requested_source,
+    B.queued_date,
+    B.start_execution_date,
+    B.last_executed_step_id,
+    B.last_executed_step_date,
+    B.stop_execution_date,
+    B.job_history_id,
+    B.next_scheduled_run_date	
+FROM
+    msdb.dbo.sysjobs A
+    JOIN msdb.dbo.sysjobactivity B ON B.job_id = A.job_id
